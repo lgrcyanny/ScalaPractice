7e8f44cc25e9714f605d771787978fa3d09fc717
lgrcyanny <lgrcyanny@gmail.com>
2017-06-12 09:00:15 +0800

    add self reference example

---
+++ ScalaPuzzlers/src/main/scala/com/learning/puzzlers/SeeSelf.scala
@@ -0,0 +1,13 @@
+package com.learning.puzzlers
+
+/**
+  * Created by lgrcyanny on 17/6/12.
+  */
+object SeeSelf {
+
+  def main(args: Array[String]): Unit = {
+    val s1: String = s1
+    val s2: String = s1 + s2
+  }
+
+}
